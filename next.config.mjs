773486a2fb87b046a4ad09561e8b753c69411850
unknown <mezzatestamatteo0103@gmail.com>
2024-07-17 16:04:44 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-// next.config.mjs
-import { withPWA } from 'next-pwa';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default withPWA({
-  pwa: {
-    dest: 'public', // la cartella dove verranno generate le risorse PWA
-    disable: process.env.NODE_ENV === 'development', // Disabilita PWA in ambiente di sviluppo
-  },
-  // Altre configurazioni Next.js normali se necessario
-});
+export default nextConfig;
