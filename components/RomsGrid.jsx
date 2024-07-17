76fc8e48b36f55cd258c96a93ac9b235f0a81ea1
unknown <mezzatestamatteo0103@gmail.com>
2024-07-17 15:23:14 +0200

    fix key missing

--- components/RomsGrid.jsx
+++ components/RomsGrid.jsx
@@ -7,7 +7,7 @@ const RomsGrid = () => {
         <h1 className='text-center text-slate-800 font-extrabold text-4xl mt-2'>Downloads</h1>
         <div className="grid grid-cols-2 gap-6 max-h-[58vh] overflow-y-auto">
       {Roms.map(rom => (
-        <div className="mt-12 flex flex-col justify-center items-center text-center" >
+        <div className="mt-12 flex flex-col justify-center items-center text-center" key={index} >
             <img src={rom.img} alt={rom.title} width={150}/>
             <p className="text-black text-sm font-bold">{rom.title}</p>
             <a href={rom.link} className="text-[#0075BE] font-bold">Scarica ora</a>
