a4021c932074f8a4b759e1b2b2ba4eb59c29d9dc
unknown <mezzatestamatteo0103@gmail.com>
2024-07-17 15:25:57 +0200

    fix

--- components/RomsGrid.jsx
+++ components/RomsGrid.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { Roms } from "@/data"
 
 
@@ -6,7 +7,7 @@ const RomsGrid = () => {
     <div className='fixed top-[18vh] left-1/2 transform -translate-x-1/2 w-[80%] pb-[20vh]'>
         <h1 className='text-center text-slate-800 font-extrabold text-4xl mt-2'>Downloads</h1>
         <div className="grid grid-cols-2 gap-6 max-h-[58vh] overflow-y-auto">
-      {Roms.map(rom => (
+      {Roms.map((rom, index) => (
         <div className="mt-12 flex flex-col justify-center items-center text-center" key={index} >
             <img src={rom.img} alt={rom.title} width={150}/>
             <p className="text-black text-sm font-bold">{rom.title}</p>
